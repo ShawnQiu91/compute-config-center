d09398e223d3a432a513d752d36a02917ad42778
TimQiu91 <123040844@qq.com>
2018-06-20 17:58:45 +0800

    first commit

---
+++ src/main/java/com/example/computeconfigcenter/ComputeConfigCenterApplication.java
@@ -0,0 +1,12 @@
+package com.example.computeconfigcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputeConfigCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputeConfigCenterApplication.class, args);
+	}
+}
